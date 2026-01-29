3b4bc04f71ad389a54d8efcdacf15accc0f36745
chandana <allu.chandu05@gmail.com>
2026-01-28 23:34:42 -0600

    actions

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please Enter your Username::"
+
+read USER_NAME
+
+echo "Username is $USER_NAME"
+
+echo " Please enter your Password::"
+
+read -s PASSWORD
+
+echo "Password is "$PASSWORD"
