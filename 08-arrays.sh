c69243a648f8ddccc99552eb442c099920d80378
chandana <allu.chandu05@gmail.com>
2026-01-29 12:38:57 -0600

    actions

---
+++ 08-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+FRUITS=("Apple" "Orange" "Grapes")
+
+echo "fruits are: ${FRUITS[@]}"
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+
