7f792c0c9e6330bddcb7faac334e32a4fd4aff73
chandana <allu.chandu05@gmail.com>
2026-01-29 16:59:50 -0600

    actions

---
+++ 10-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ USERID -ne 0]; then
+    echo " please run this script with root user access"
+    exit 1
+fi
+
+    echo "insatalling Nginx"
+    dnf install nginx -y
+
+if [$? -ne 0]; then
+    echo "installing nginx ... FAILURE"
+    exit 1
+
+elif
+    echo "Installing nginx ... SUCCESS"
+fi
