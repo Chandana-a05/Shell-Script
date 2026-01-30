5d6b972ba1c79eceb26832b4cd5888215a8fafbb
chandana <allu.chandu05@gmail.com>
2026-01-30 13:09:49 -0600

    actions

---
+++ 11-funtions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo " please run this script with root user access"
+    exit 1
+fi  
+VALIDATION (){
+    if [$1 -ne 0]; then
+        echo "$2 ......FAILURE"
+        exit1
+    else
+         echo " $2..... SUCCESS"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
+
+dnf install nodejs -y
+ VALIDATE $? "Installing Nodejs"
